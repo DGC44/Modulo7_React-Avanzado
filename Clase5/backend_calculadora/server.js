dea91bead3d6e073bb5064a0d9fb9030147967c4
DianaGC <garcapdia@hotmail.com>
2025-07-28 17:03:27 -0600

    Clase 5

---
+++ Clase5/backend_calculadora/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const server = express()
+const PORT = 3000
+
+server.use(express.json()) // middleware
+
+function calcular(operation, A, B) {
+    switch(operation) {
+        case "restar":
+            return  A - B
+        case "sumar":
+            return A + B
+        case "multiplicar":
+            return A * B
+        case "dividir":
+            return A / B
+    }
+}
+
+server.get('/', (req, res) => {
+    res.send("Probando...")
+})
+
+// URL params /:operation/:valA/:valB  con GET
+server.get('/:operation/:valA/:valB',(req, res) => {
+    const operation = req.params.operation
+    const A = Number(req.params.valA)
+    const B = Number(req.params.valB)
+
+    res.send(`El resultado de ${operation} de ${A} y ${B} es ${calcular(operation, A, B)} `)
+})
+
+
+// POST params (JSON data)
+// { "operation" : "suma", valA: 1, valB: 2 } Con POST, raw
+server.post("/calcular", (req, res) => {
+    const operation = req.body.operation
+    const A = Number(req.body.valA)
+    const B = Number(req.body.valB)
+
+    res.send(`El resultado de ${operation} de ${A} y ${B} es ${calcular(operation, A, B)}`)
+})
+
+// Query params
+// /calcular?operation=restar&valA=1&valB=2
+server.get("/calcular",(req, res) => {
+    const operation = req.query.operation
+    const A = Number(req.query.valA)
+    const B = Number(req.query.valB)
+
+    res.send(`El resultado de ${operation} de ${A} y ${B} es ${calcular(operation, A, B)}`)
+})
+
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})

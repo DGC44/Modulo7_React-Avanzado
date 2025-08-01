0feb5d7b4c50e6b91610e2ced039a0e86fdc2fdb
DianaGC <garcapdia@hotmail.com>
2025-07-31 21:00:52 -0600

    Clase 7

--- Clase7/clase.md
+++ Clase7/clase.md
@@ -4,3 +4,9 @@
 - Conectar una app React a una base de datos remota.
 - Realizar operaciones CRUD en tiempo real o por API.
 - Usar autenticación básica con estos servicios.
+
+
+## Bases de datos
+-  Relacional - RDS (relaciones por medio de llaves. Varias tareas del mismo usuario. Muchas relaciones. Usado en ambientes complejos) Ejemplo supabase Base de documentos 
+
+- NOSQL (relaciones de forma anidada. to do's. Objeto literal de cada TO DO.Usos mas de prototipos, informacion dinamica y cambiante) Ejemplo: firabase google
---
+++ Clase7/ejemplo_firabase/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJSkzn11h1vKrJOkjptL7k5KZowJPOzi8",
+  authDomain: "devf-d1168.firebaseapp.com",
+  projectId: "devf-d1168",
+  storageBucket: "devf-d1168.firebasestorage.app",
+  messagingSenderId: "550671841531",
+  appId: "1:550671841531:web:cec37bc3e091af4d72d85e",
+  measurementId: "G-VJHQH77KZ2"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase
+
